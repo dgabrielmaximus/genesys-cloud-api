dd878401dd70b787e9b1b7ae0a812312d84b07c9
Dianne Gabriel <dianne.gabriel@maximuscanada.ca>
2024-02-06 10:28:59 -0500

    Remove env

--- routingApi_postRoutingWrapupcodes.js
+++ routingApi_postRoutingWrapupcodes.js
@@ -13,12 +13,32 @@ const routingApi = new platformClient.RoutingApi();
 
 let body = {}; // Object | WrapupCode
 
-// Create a wrap-up code
-routingApi.postRoutingWrapupcodes(body)
+// Authenticate
+client
+  .loginClientCredentialsGrant(clientId, clientSecret)
+  .then(() => {
+    return routingApi.postRoutingWrapupcodes(body);
+  })
+  // Create a wrap-up code
   .then((data) => {
-    console.log(`postRoutingWrapupcodes success! data: ${JSON.stringify(data, null, 2)}`);
+    console.log(
+      `postRoutingWrapupcodes success! data: ${JSON.stringify(data, null, 2)}`
+    );
   })
   .catch((err) => {
     console.log("There was a failure calling postRoutingWrapupcodes");
     console.error(err);
   });
+
+/*
+{
+  "id": "24a11d9e-e7b0-4587-b486-df72383f5266",
+  "name": "Sample|Test",
+  "division": {
+    "id": "*"
+  },
+  "dateCreated": "2024-02-06T15:19:28.722Z",
+  "createdBy": "db7b7410-4748-409c-89fa-7c8cd9755cc5",
+  "selfUri": "/api/v2/routing/wrapupcodes/24a11d9e-e7b0-4587-b486-df72383f5266"
+}
+  */
